4debc171ef47d23d1e6eb30ec2af74aefdfdae72
Tanakorn Phoochaliaw <tanakorn.dev@gmail.com>
2022-08-20 09:59:47 +0700

    add news project

---
+++ News-iOS/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  News-iOS
+//
+//  Created by Tanakorn Phoochaliaw on 19/8/2565 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
